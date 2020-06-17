14cbba061259936b19458dd3bc818109d6723e09
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-17 04:11:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   
   attr_accessor :id, :name, :breed
   
-  def initialize(attributes)
-    attributes.each{|key, value| self.send(("#{key}="), value)}
+  def initialize(attrs)
+    attrs.each{|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
   
@@ -22,6 +22,45 @@ class Dog
   end
   
   def save
-    sql = 
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+  
+  def self.create(attrs)
+    dog = self.new(attrs)
+    dog.save
+  end
+  
+  def self.new_from_db(row)
+    attrs = {
+      :id => row[0]
+      :name => row[1]
+      :breed => row[2]
+    }
+    self.new(attributes)
+  end
+  
+  def self.find_by_id(x)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    DB[:conn].execute(sql, x)
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    DB[:conn].execute(sql, name, breed).first
+    
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
   
 end
